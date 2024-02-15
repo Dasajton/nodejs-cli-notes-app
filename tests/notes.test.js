d6ce649e8b5a4a0eb21dc1b5cf741502c9ab456a
Dasajton <david.sajitz.94@gmail.com>
2024-02-15 18:18:29 +0100

    tests grouped

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -16,61 +16,63 @@ beforeEach(() => {
   saveDB.mockClear();
 });
 
-// Test for: newNote()
-test("newNote inserts data and returns it", async () => {
-  const note = {
-    id: Date.now(),
-    content: "Test note",
-    tags: ["test"],
-  };
-  insertDB.mockResolvedValue(note);
+describe("cli notes app", () => {
+  // Test for: newNote()
+  test("newNote inserts data and returns it", async () => {
+    const note = {
+      id: Date.now(),
+      content: "Test note",
+      tags: ["test"],
+    };
+    insertDB.mockResolvedValue(note);
 
-  const result = await newNote(note.content, note.tags);
-  expect(result).toEqual(note);
-});
+    const result = await newNote(note.content, note.tags);
+    expect(result).toEqual(note);
+  });
 
-// Test for: getAllNotes()
-test("getAllNotes returns all notes", async () => {
-  const db = {
-    notes: ["note1", "note2", "note3"],
-  };
-  getDB.mockResolvedValue(db);
+  // Test for: getAllNotes()
+  test("getAllNotes returns all notes", async () => {
+    const db = {
+      notes: ["note1", "note2", "note3"],
+    };
+    getDB.mockResolvedValue(db);
 
-  const result = await getAllNotes();
-  expect(result).toEqual(db.notes);
-});
+    const result = await getAllNotes();
+    expect(result).toEqual(db.notes);
+  });
 
-// Test for: findNotes()
-test("findNotes returns notes that match the filter", async () => {
-  const notes = [
-    { id: 1, content: "note 1" },
-    { id: 2, content: "note 2" },
-    { id: 3, content: "note 3" },
-  ];
-  getDB.mockResolvedValue({ notes });
+  // Test for: findNotes()
+  test("findNotes returns notes that match the filter", async () => {
+    const notes = [
+      { id: 1, content: "note 1" },
+      { id: 2, content: "note 2" },
+      { id: 3, content: "note 3" },
+    ];
+    getDB.mockResolvedValue({ notes });
 
-  const result = await findNotes("2");
-  expect(result).toEqual([notes[1]]);
-});
+    const result = await findNotes("2");
+    expect(result).toEqual([notes[1]]);
+  });
 
-// Test for: removeNote()
-test("removeNote does nothing if id is not found", async () => {
-  const notes = [
-    { id: 1, content: "note 1" },
-    { id: 2, content: "note 2" },
-    { id: 3, content: "note 3" },
-  ];
-  saveDB.mockResolvedValue(notes);
+  // Test for: removeNote()
+  test("removeNote does nothing if id is not found", async () => {
+    const notes = [
+      { id: 1, content: "note 1" },
+      { id: 2, content: "note 2" },
+      { id: 3, content: "note 3" },
+    ];
+    saveDB.mockResolvedValue(notes);
 
-  const idToRemove = 4;
-  const result = await removeNote(idToRemove);
-  expect(result).toBeUndefined();
-});
+    const idToRemove = 4;
+    const result = await removeNote(idToRemove);
+    expect(result).toBeUndefined();
+  });
 
-// Test for: removeAllNotes()
-test("removeAllNotes removes all notes", async () => {
-  saveDB.mockResolvedValue({ notes: [] });
+  // Test for: removeAllNotes()
+  test("removeAllNotes removes all notes", async () => {
+    saveDB.mockResolvedValue({ notes: [] });
 
-  const result = await removeAllNotes();
-  expect(result).toEqual({ notes: [] });
+    const result = await removeAllNotes();
+    expect(result).toEqual({ notes: [] });
+  });
 });
